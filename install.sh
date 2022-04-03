4b61ae4236708a107a4a6f9cc54e702b9741673f
Owen Brooks <owen.h.brooks@gmail.com>
2022-04-04 09:28:36 +1000

    Add install script to the repository
    Will make it easier to provide a download link

---
+++ install.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+
+#install ros2
+locale  # check for UTF-8
+
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+locale  # verify settings
+
+sudo apt update && sudo apt install curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+
+sudo apt install ros-foxy-desktop
+
+#install vscode
+sudo apt update
+sudo apt install software-properties-common apt-transport-https wget
+
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt install code
+
+sudo apt update
+sudo apt upgrade
+
+#install colcon
+sudo apt install python3-colcon-common-extensions
+
+#install rosdep
+sudo apt install python3-rosdep2
+rosdep update
+
+#install gazebo
+sudo apt install ros-foxy-gazebo-ros-pkgs
+
+echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
