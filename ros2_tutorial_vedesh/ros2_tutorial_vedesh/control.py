138eb94ab8e3ac8963c4a6aea1e118813980f6e4
Vedesh <vedesh1408@gmail.com>
2023-04-11 14:56:14 +1000

    Changed twist to twistStamped

--- ros2_tutorial_vedesh/ros2_tutorial_vedesh/control.py
+++ ros2_tutorial_vedesh/ros2_tutorial_vedesh/control.py
@@ -9,8 +9,7 @@ class ControlNode(Node):
 
     def __init__(self):
         super().__init__('control_node')
-        self.publisher = self.create_publisher(
-            Twist, '/simulated_vehicle/cmd_vel', 10)
+        self.publisher_ = self.create_publisher(TwistStamped, '/simulated_vehicle/cmd_vel', 10)
         self.subscription = self.create_subscription(
             Bool, '/ebrake_is_active', self.listener_callback, 10)
         self.emergency_brake = False
@@ -20,14 +19,14 @@ class ControlNode(Node):
         self.emergency_brake = msg.data # Gets brake data
 
     def publish_twist(self):
-        msg = Twist()
+        msg = TwistStamped()
         if not self.emergency_brake:
-            msg.linear.x = 2.0  # forward
+            msg.twist.linear.x = 2.0  # forward
             self.publisher.publish(msg)
   
         else:
-            msg.linear.x = 0.0
-            msg.angular.z = 0.0
+            msg.twist.linear.x= 0.0
+            msg.twist.angular.x = 0.0
             print("Stop")
             self.publisher.publish(msg)
 
